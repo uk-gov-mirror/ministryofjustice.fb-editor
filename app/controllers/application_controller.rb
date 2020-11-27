ed045090fc7fad597ade5091af7acb5a3c1d9ae4
Tomas D'Stefano <tomas_stefano@successoft.com>
2020-11-27 17:50:13 +0000

    Save user data in the session by service
    Now we are saving the user data in the session
    
    the format is the service name and 'user_data' as the key.
    
    e.g service "complain-about-a-court" the session will be:
    
    { 'complain-about-a-court' => { 'user_data' => { }  } }

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,23 +1,29 @@
 class ApplicationController < ActionController::Base
-  def service_metadata
-    JSON.parse(File.read(Rails.root.join('service.json')))
-  end
-
   def service
     @service ||= MetadataPresenter::Service.new(service_metadata)
   end
 
-  def save_user_data	    
-    if params[:answers]
-      session[:user_data] ||= {}
+  def save_user_data
+    return {} if params[:answers].blank? || params[:service_id].blank?
 
-      params[:answers].each do |field, answer|
-        session[:user_data][field] = answer
-      end
+    service_id = params[:service_id]
+    session[service_id] ||= {}
+    session[service_id]['user_data'] ||= {}
+
+    params[:answers].each do |field, answer|
+      session[service_id]['user_data'][field] = answer
     end
   end
 
   def load_user_data
-    session[:user_data] || {}
+    user_data = session[params[:service_id]] || {}
+
+    user_data['user_data'] || {}
+  end
+
+  private
+
+  def service_metadata
+    JSON.parse(File.read(Rails.root.join('service.json')))
   end
 end
