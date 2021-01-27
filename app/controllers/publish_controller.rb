b45bd911fdb8bd0c783c44fee9533c94b8ac69dd
Tomas D'Stefano <tomas_stefano@successoft.com>
2021-01-27 16:34:26 -0300

    Add publish page
    This is the part 1 of the publishing mechanism. This part involves
    just adding the interface and preparing the cloud platform
    setup in order for the editor to have permissions to provision
    pods in the services namespace.
    
    This adds the model (and the db table) without index just yet,
    the controller and the empty job - without any job adapter yet.

---
+++ app/controllers/publish_controller.rb
@@ -0,0 +1,16 @@
+class PublishController < FormController
+  def index
+    @publish_service = PublishService.new
+  end
+
+  def create
+    @publish_service = PublishService.new(
+      params.require(:publish_service).permit(
+        :deployment_environment
+      ).merge(service_id: service.service_id, status: :queued)
+    )
+
+    @publish_service.save!
+    PublishServiceJob.perform_later(publish_service_id: @publish_service.id)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_04_163441) do
+ActiveRecord::Schema.define(version: 2021_01_27_190927) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -25,6 +25,14 @@ ActiveRecord::Schema.define(version: 2021_01_04_163441) do
     t.index ["email"], name: "index_identities_on_email"
     t.index ["provider", "uid"], name: "index_identities_on_provider_and_uid"
     t.index ["user_id"], name: "index_identities_on_user_id"
+  end
+
+  create_table "publish_services", force: :cascade do |t|
+    t.string "deployment_environment", null: false
+    t.uuid "service_id", null: false
+    t.string "status", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
