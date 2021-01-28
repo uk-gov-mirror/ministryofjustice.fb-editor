abaed14ab4b79f779a38e930c75b0b4543d0b29d
Steven Burnell <steven.burnell@digital.justice.gov.uk>
2021-01-28 18:19:08 +0000

    Rework dialog JS to make it reusable for other pages

--- app/javascript/services/index.js
+++ app/javascript/services/index.js
@@ -1,48 +1,74 @@
-// console.log("app/javascript/services/index.js");
+// Always runs when document ready.
+//
 $(document).ready(function() {
-  applyDialog();
+  applyFormDialogs();
 });
 
-// Creates the modal dialog effect for the new form input
-// TODO: Extract the text to pull in from HTML+JS mechanism.
-function applyDialog() {
-  var $dialog = $("#new-service-create-dialog");
-  var $activator = $("<button class=\"govuk-button\">\</button>");
-  var $form = $("#new_service_creation");
 
-  if($dialog.length) {
-    $dialog.dialog({
-      autoOpen: false,
-      buttons: {
-        "Create a new form": function() {
-          $form.submit();
-          $( this ).dialog( "close" );
-        },
-        Cancel: function() {
-          $( this ).dialog( "close" );
-        }
-      },
-      classes: {
-        "ui-button": "govuk-button"
-      },
-      closeOnEscape: false,
-      height: "auto",
-      modal: true,
-      resizable: false
-    });
+// Finds forms structured to become dialog effects and
+// wraps them with the required functionality.
+//
+function applyFormDialogs() {
+	$(".component-dialog-form").each(function(i, el) {
+    var $dialog = $(el);
+    var $form = $dialog.find("form");
 
     // Design requires an activator be added dynamically.
-	  $activator.text("Create a new form");
-    $activator.button().on( "click", function() {
-      $dialog.dialog( "open" );
+    // Create and place it before enhancing the dialog
+    // because that functionality will move and hide the
+    // dialog element.
+    $dialog.before(createDialogActivator($dialog, $dialog.data("activator-text")));
+
+    // Enhance target element with desired functionality.
+    createFormDialog($dialog, {
+      form: $form,
+      cancel_text: $dialog.data("cancel-text"),
+      submit_text: $form.find(":submit").val()
     });
+  });
+}
 
-    $(".fb-editor-form-list").after($activator);
 
-    // POST will redirect to same page but with errors,
-    // so we need to auto-open the dialog for user to see.
-    if($dialog.find(".govuk-form-group--error").length) {
-      $dialog.dialog("open");
-    }
+// Creates a buttons and links with the passed dialog element.
+// @$dialog (jQuery object) Target dialog element enhanced with dialog funcitonality.
+// @text    (String) Text that will show on the button.
+function createDialogActivator($dialog, text) {
+  return $("<button class=\"govuk-button\">\</button>").on( "click", function() {
+             $dialog.dialog( "open" );
+           }).text(text);
+}
+
+// Enhances a dialog wrapped form with required (popup) functionlity.
+// @dialog (Node) Target element that will become the dialog.
+// @config (Object) Properties and elements that will be required for dialog build.
+//
+function createFormDialog($dialog, config) {
+  var buttons = {}
+
+  buttons[config.submit_text] = function() {
+    config.$form.submit();
+    $(this).dialog( "close" );
+  }
+
+  buttons[config.cancel_text] = function() {
+    $(this).dialog("close");
+  }
+
+  $dialog.dialog({
+    autoOpen: false,
+    buttons: buttons,
+    classes: {
+      "ui-button": "govuk-button"
+    },
+    closeOnEscape: false,
+    height: "auto",
+    modal: true,
+    resizable: false
+  });
+
+  // POST will redirect to same page but with errors,
+  // so we need to auto-open the dialog for user to see.
+  if($dialog.find(".govuk-form-group--error").length) {
+    $dialog.dialog("open");
   }
 }
