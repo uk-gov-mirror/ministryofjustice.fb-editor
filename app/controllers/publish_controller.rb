8a9922b4c2e2e5bc939853b460e4b4da0b84bd31
Tomas D'Stefano <tomas_stefano@successoft.com>
2021-02-05 11:57:04 -0300

    Fix bug on publishing form
    When the user add invalid username and the invalid username should
    be displayed on the form with the validation message inline
    
    Also fix the bug of duplicated validation messages between
    deploy to test (dev) deploy to live (production - OMG those names)
    so we separated into two form objects - one for each environment
    but when we save (only one of them) we need to re-render both of
    them that's the reason we need to copy from the publish service creation
    into the dev or production form objects (see update_form_objects
    controller method for more details)
    
    Co-authored-by: Natalie Seeto <natalie.seeto@digital.justice.gov.uk>
    Co-authored-by: Brendan Butler <brendan.butler@digital.justice.gov.uk>

--- app/controllers/publish_controller.rb
+++ app/controllers/publish_controller.rb
@@ -1,7 +1,5 @@
 class PublishController < FormController
-  def index
-    @publish_service_creation = PublishServiceCreation.new(service_id: service.service_id)
-  end
+  before_action :assign_form_objects
 
   def create
     @publish_service_creation = PublishServiceCreation.new(publish_service_params)
@@ -10,7 +8,9 @@ class PublishController < FormController
       PublishServiceJob.perform_later(
         publish_service_id: @publish_service_creation.publish_service_id
       )
+      redirect_to publish_index_path(service.service_id)
     else
+      update_form_objects
       render :index
     end
   end
@@ -24,5 +24,24 @@ class PublishController < FormController
       :password,
       :deployment_environment
     ).merge(service_id: service.service_id)
+  end
+
+  def assign_form_objects
+    @publish_service_creation_dev = PublishServiceCreation.new(
+      service_id: service.service_id,
+      deployment_environment: 'dev'
+    )
+    @publish_service_creation_production = PublishServiceCreation.new(
+      service_id: service.service_id,
+      deployment_environment: 'production'
+    )
+  end
+
+  def update_form_objects
+    if @publish_service_creation.deployment_environment == 'dev'
+      @publish_service_creation_dev = @publish_service_creation
+    else
+      @publish_service_creation_production = @publish_service_creation
+    end
   end
 end
