86c98f454ec72b366c3f07cef8d8d35d141e3d52
brenetic <brendan.butler@digital.justice.gov.uk>
2021-03-11 11:27:06 +0000

    Encrypt user details in DB
    Co-authored-by: Tomas Destefi <tomas.destefi@digital.justice.gov.uk>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   has_many :identities, dependent: :destroy
 
+  include UserEncryption
+
+  before_save :encrypt_attributes
+
   def has_identity?(identity)
     identities.any? do |id|
       id.uid == identity.uid &&
