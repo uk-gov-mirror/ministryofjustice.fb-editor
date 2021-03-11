f000156f9f5957ea51c8187687225f517e38b833
Steven Burnell <steven.burnell@digital.justice.gov.uk>
2021-03-11 10:46:57 +0000

    Fix multiple heading title output

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,11 +6,16 @@ module ApplicationHelper
   # of the template.
   def flow_thumbnail(page, parent_id)
     if page.components.nil? || page.components.empty?
-      type = page._type.gsub("page.", "")
+      type = page._type.gsub('page.', '')
       heading = page.heading
     else
       type = page.components[0]._type
-      heading = page.components[0].label || page.components[0].legend
+
+      if page._type == 'page.multiplequestions'
+        heading = page.heading
+      else
+        heading = page.components[0].label || page.components[0].legend
+      end
     end
     link_to edit_page_path(parent_id, page.uuid), class: "form-step_thumbnail #{type}", 'aria-hidden': true do
       concat image_pack_tag('thumbnails/thumbs_header.png', class: 'header')
