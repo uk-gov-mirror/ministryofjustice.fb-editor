e2d4c628bc000af484adfa43154427d4e4d9221e
brenetic <brendan.butler@digital.justice.gov.uk>
2021-03-17 23:51:04 +0000

    Add default text ability
    Ensure the default text is editable and is only shown in preview mode when the user has customised it.
    
    Story: https://trello.com/c/YSHgwzER/1382-placeholder-text
    
    Co-authored-by: Brendan Butler <brendan.butler@digital.justice.gov.uk>
    Co-authored-by: Tomas Destefi <tomas.destefi@digital.justice.gov.uk>

--- app/controllers/preview_controller.rb
+++ app/controllers/preview_controller.rb
@@ -1,3 +1,8 @@
 class PreviewController < ApplicationController
   layout 'metadata_presenter/application'
+
+  def service
+    @service ||= MetadataPresenter::Service.new(service_metadata, editor: editable?)
+  end
+  helper_method :service
 end
