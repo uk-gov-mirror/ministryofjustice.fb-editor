deb925cd49b1f8c954283fcd47047d119b74d186
Tomas D'Stefano <tomas_stefano@successoft.com>
2021-03-19 12:04:48 -0300

    Remove service method
    It is the same as app controller

--- app/controllers/preview_controller.rb
+++ app/controllers/preview_controller.rb
@@ -1,8 +1,3 @@
 class PreviewController < ApplicationController
   layout 'metadata_presenter/application'
-
-  def service
-    @service ||= MetadataPresenter::Service.new(service_metadata, editor: editable?)
-  end
-  helper_method :service
 end
