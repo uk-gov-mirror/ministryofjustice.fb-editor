8a5b29b5ce0b7f9e1fc8d2f630edcb855668edd4
Tomas D'Stefano <tomas_stefano@successoft.com>
2021-03-30 10:36:54 -0300

    Don't show the delete page if is the start page
    On the backend we don't allow start pages to be deleted so the
    "Delete page" on start page is useless.

--- acceptance/features/deleting_page_spec.rb
+++ acceptance/features/deleting_page_spec.rb
@@ -10,13 +10,6 @@ feature 'Deleting page' do
     given_I_have_a_service
   end
 
-  scenario 'when deleting start page' do
-    given_I_want_to_delete_the_start_page
-    when_I_delete_the_page
-    and_I_return_to_flow_page
-    then_I_should_still_see_the_start_page # Do not delete precious start pages
-  end
-
   scenario 'when deleting other pages' do
     given_I_have_a_single_question_page_with_text
     and_I_return_to_flow_page
@@ -24,11 +17,6 @@ feature 'Deleting page' do
     when_I_delete_the_page
     and_I_return_to_flow_page
     then_I_should_not_see_the_deleted_page_anymore
-  end
-
-  def given_I_want_to_delete_the_start_page
-    editor.preview_page_images.first.hover
-    editor.three_dots_button.click
   end
 
   def and_I_want_to_delete_the_page_that_I_created
@@ -39,10 +27,6 @@ feature 'Deleting page' do
   def when_I_delete_the_page
     editor.delete_page_link.click
     editor.delete_page_modal_button.click
-  end
-
-  def then_I_should_still_see_the_start_page
-    expect(editor.form_urls.map(&:text)).to eq(['/'])
   end
 
   def then_I_should_not_see_the_deleted_page_anymore
